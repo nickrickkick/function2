200260e44e10fa10ebeb6c424ed6c54cf0c96b19
Michael Nowlin <michaelnowlin5@gmail.com>
2022-03-10 18:59:17 -0700

    getting the hang of these arraymethods

--- arrayMethods.js
+++ arrayMethods.js
@@ -19,7 +19,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 
 // CODE HERE
 const evenNumbers  = mixedNumbers.filter(even => even % 2 === 0);
-console.log(evenNumbers);
+//console.log(evenNumbers);
 
 
 
@@ -40,7 +40,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-//const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices  = prices.map(afterTax => afterTax = afterTax * 1.07);
+//console.log(postTaxPrices);
 
 
 
@@ -58,7 +59,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-//const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+const totalPopulation   = populations.reduce((acc, curr) => acc + curr);
+console.log(totalPopulation);
 
 
 
